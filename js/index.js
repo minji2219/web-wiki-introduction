529af0c2dac2204f7f6025a10384ded72776d364
minji2219 <aabb0219@naver.com>
2025-02-27 20:57:25 +0900

    feat: 댓글 입력 동작 추가

--- js/index.js
+++ js/index.js
@@ -1,19 +1,29 @@
-console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
-    "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
-    "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
-);
+document.querySelector('.register').addEventListener('click', () => {
+  const input = document.querySelector('.review input');
 
-console.log("알맞은 스크립트를 작성하세요");
+  if (input.value === '') {
+    alert('댓글을 작성해주세요.');
+    return;
+  }
+
+  const commentListEl = document.querySelector('.comment-list');
+  const comment = document.createElement('li');
+  comment.innerHTML = `
+      <div class="comment-item">
+        <div class="comment-author">
+          <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+          <span class="visitor">방문자</span>
+        </div>
+        <div class="comment-content">
+          ${input.value}
+        </div>
+      </div>
+  `;
+  commentListEl.prepend(comment);
+
+  input.value = '';
+});
+
+document.querySelector('.cancel').addEventListener('click', () => {
+  document.querySelector('.review input').value = '';
+});
